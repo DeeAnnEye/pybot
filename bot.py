3a5d9c420233cce6526542265896a2588652c52c
Justin Antony <danielashulamithjustin@gmail.com>
2021-01-01 18:37:28 +0530

    Added direct message function

--- bot.py
+++ bot.py
@@ -11,13 +11,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server and HAVE FUN!'
     )
 
+    
 client.run(TOKEN)
