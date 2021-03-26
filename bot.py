e12dd6b4cb6f97eb1e689d15d2725045cd851c04
Justin Antony <danielashulamithjustin@gmail.com>
2021-03-26 21:07:57 +0530

    Updated bot on event handling methods

--- bot.py
+++ bot.py
@@ -3,15 +3,18 @@ import os
 import discord
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+ 
 
 @client.event
 async def on_member_join(member):
@@ -20,5 +23,15 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server and HAVE FUN!'
     )
 
-    
+
+@client.event
+async def on_message(message):
+    # don't respond to ourselves
+    if message.author == client.user:
+        return
+
+    if message.content == 'hello':
+        await message.channel.send('hello world')
+
+
 client.run(TOKEN)
